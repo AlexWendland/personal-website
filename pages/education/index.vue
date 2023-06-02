2d3f953d311c56769ead9c63a70979e944018e2f
Alex Wendland <a.p.wendland@gmail.com>
2023-06-02 07:53:15 +0100

    Add academic picture

--- pages/education/index.vue
+++ pages/education/index.vue
@@ -1,1 +1,9 @@
-<template></template>
+<template>
+  <div class="grid grid-cols-2 p-6">
+    <div>
+      <h2 class="font-bold block text-xl text-center p-4">Education</h2>
+      <p class="block text-xl p-1"></p>
+    </div>
+    <img src="~/assets/images/academic.jpg" alt="An extraordinarily intelligent man." class="rounded-xl shadow-xl" />
+  </div>
+</template>
