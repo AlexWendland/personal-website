b901e1383967a98d9eb75b378e3a338763501154
Alex Wendland <a.p.wendland@gmail.com>
2023-06-14 10:10:00 +0000

    Make projects page filterable

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,14 +3,15 @@
     <h3>Filter</h3>
     <div class="flex flex-wrap gap-2">
       <button
-        v-for="skill in skills"
+        v-for="[skill_key, skill] in Object.entries(skills)"
         :key="skill.name"
-        class="rounded-md bg-gray-800 px-2 py-1 text-sm text-white"
-        :class="{ 'bg-gray-600': skillsFilter.includes(skill.name) }"
+        class="rounded-md bg-gray-600 px-2 py-1 text-sm text-white"
+        :class="{ 'bg-emerald-600': skillsFilter.includes(skill_key) }"
         @click="
-          skillsFilter.includes(skill.name)
-            ? skillsFilter.splice(skillsFilter.indexOf(skill.name), 1)
-            : skillsFilter.push(skill.name);
+          skillsFilter.includes(skill_key)
+            ? skillsFilter.splice(skillsFilter.indexOf(skill_key), 1)
+            : skillsFilter.push(skill_key);
+          console.log(skillsFilter);
           filterProjects();
         "
       >
@@ -29,14 +30,11 @@
 import projects from "~/data/projects.json";
 import skills from "~/data/skills.json";
 const project_list = Object.values(projects);
-var skillsFilter = [];
-const filteredProjects = useState("filteredProjects", () =>
-  Object.values(projects)
-);
-
+const skillsFilter = ref([]);
+const filteredProjects = ref(project_list);
 const filterProjects = () => {
-  filteredProjects = project_list.filter((project) => {
-    return skillsFilter.every((skill) => project.skills.includes(skill));
+  filteredProjects.value = project_list.filter((project) => {
+    return skillsFilter.value.every((skill) => project.skills.includes(skill));
   });
 };
 </script>
