8387b2a63cdb7300f86bd0734c8db3ae5b4599a5
Alex Wendland <a.p.wendland@gmail.com>
2023-06-14 07:50:49 +0000

    Start trying to make the projects page reactive

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,25 @@
 <template>
+  <div class="container">
+    <h3>Filter</h3>
+    <div class="flex flex-wrap gap-2">
+      <button
+        v-for="skill in skills"
+        :key="skill.name"
+        class="rounded-md bg-gray-800 px-2 py-1 text-sm text-white"
+        :class="{ 'bg-gray-600': skillsFilter.includes(skill.name) }"
+        @click="
+          skillsFilter.includes(skill.name)
+            ? skillsFilter.splice(skillsFilter.indexOf(skill.name), 1)
+            : skillsFilter.push(skill.name);
+          filterProjects();
+        "
+      >
+        {{ skill.name }}
+      </button>
+    </div>
+  </div>
   <div class="grid justify-center gap-5 md:grid-cols-2">
-    <div v-for="project in projects">
+    <div v-for="project in filteredProjects">
       <ProjectCard :project="project" />
     </div>
   </div>
@@ -8,4 +27,16 @@
 
 <script setup>
 import projects from "~/data/projects.json";
+import skills from "~/data/skills.json";
+const project_list = Object.values(projects);
+var skillsFilter = [];
+const filteredProjects = useState("filteredProjects", () =>
+  Object.values(projects)
+);
+
+const filterProjects = () => {
+  filteredProjects = project_list.filter((project) => {
+    return skillsFilter.every((skill) => project.skills.includes(skill));
+  });
+};
 </script>
---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p></p>
+    Counter: {{ counter }}
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </div>
+</template>
+
+<script setup>
+const counter = useState("counter", () => Math.round(Math.random() * 1000));
+</script>
