c80a03b1b02722c681b52fbd247ac09a359e3168
Alex Wendland <a.p.wendland@gmail.com>
2023-06-16 05:54:51 +0000

    Neaten up papers page

--- pages/papers.vue
+++ pages/papers.vue
@@ -1,71 +1,99 @@
 <template>
   <h3 class="block p-4 text-center text-xl font-bold">Papers</h3>
-  <ul class="list-disc pl-4">
-    <li>
-      <a
-        class="link-primary link"
-        href="https://homepages.warwick.ac.uk/~maslar/"
-        >Agelos Georgakopoulos</a
-      >, Alex Wendland.
+  <div class="grid gap-4">
+    <div class="rounded bg-base-200 p-2">
+      <h2 class="text-center text-xl">
+        <a href="https://arxiv.org/abs/2304.13317"
+          >A study of 2-ended graphs via harmonic functions</a
+        >
+      </h2>
+      <p class="p-2">
+        <a class="link" href="https://homepages.warwick.ac.uk/~maslar/"
+          >Agelos Georgakopoulos</a
+        >, Alex Wendland.
+      </p>
       <a class="link-primary link" href="https://arxiv.org/abs/2304.13317"
-        >A study of 2-ended graphs via harmonic functions</a
-      >. Pre-print, April 2023.
-    </li>
-    <li>
-      <a
-        class="link-primary link"
-        href="https://warwick.ac.uk/fac/sci/statistics/staff/research_students/morina/"
-        >Giulio Morina</a
-      >,
-      <a
-        class="link-primary link"
-        href="https://warwick.ac.uk/fac/sci/statistics/staff/academic-research/latuszynski/"
-        >Krzysztof Łatuszyński</a
-      >,
-      <a class="link-primary link" href="https://www.mimuw.edu.pl/~nayar/"
-        >Piotr Nayar</a
-      >, Alex Wendland.
+        >Pre-print</a
+      >, April 2023.
+    </div>
+
+    <div class="rounded bg-base-200 p-2">
+      <h2 class="text-center text-xl">
+        <a
+          href="https://projecteuclid.org/journals/annals-of-applied-probability/volume-32/issue-1/From-the-Bernoulli-factory-to-a-dice-enterprise-via-perfect/10.1214/21-AAP1679.short"
+          >From the Bernoulli factory to a dice enterprise via perfect sampling
+          of Markov chains</a
+        >
+      </h2>
+      <p class="p-2">
+        <a
+          class="link"
+          href="https://warwick.ac.uk/fac/sci/statistics/staff/research_students/morina/"
+          >Giulio Morina</a
+        >,
+        <a
+          class="link"
+          href="https://warwick.ac.uk/fac/sci/statistics/staff/academic-research/latuszynski/"
+          >Krzysztof Łatuszyński</a
+        >,
+        <a class="link" href="https://www.mimuw.edu.pl/~nayar/">Piotr Nayar</a>,
+        Alex Wendland.
+      </p>
       <a
         class="link-primary link"
         href="https://projecteuclid.org/journals/annals-of-applied-probability/volume-32/issue-1/From-the-Bernoulli-factory-to-a-dice-enterprise-via-perfect/10.1214/21-AAP1679.short"
-        >From the Bernoulli factory to a dice enterprise via perfect sampling of
-        Markov chains</a
-      >. Annals of Applied Probability, Volume 32, Issue 1, Pages 327-359,
-      February 2022.
-    </li>
-    <li>
-      <a
-        class="link-primary link"
-        href="https://homepages.warwick.ac.uk/~maslar/"
-        >Agelos Georgakopoulos</a
-      >, Alex Wendland.
+        >Annals of Applied Probability</a
+      >, Volume 32, Issue 1, Pages 327-359, February 2022.
+    </div>
+    <div class="rounded bg-base-200 p-2">
+      <h2 class="text-center text-xl">
+        <a href="https://link.springer.com/article/10.1007/s10801-021-01070-6"
+          >Presentations for vertex-transitive graphs</a
+        >
+      </h2>
+      <p class="p-2">
+        <a class="link" href="https://homepages.warwick.ac.uk/~maslar/"
+          >Agelos Georgakopoulos</a
+        >, Alex Wendland.
+      </p>
       <a
         class="link-primary link"
         href="https://link.springer.com/article/10.1007/s10801-021-01070-6"
-        >Presentations for vertex-transitive graphs</a
-      >. Journal of Algebraic Combinatorics, Volume 55, Pages 795-826, 4 October
-      2021.
-    </li>
-    <li>
-      <a
-        class="link-primary link"
-        href="https://warwick.ac.uk/fac/sci/maths/people/staff/dmitriy_rumynin/"
-        >Dmitriy Rumynin</a
-      >, Alex Wendland.
+        >Journal of Algebraic Combinatorics</a
+      >, Volume 55, Pages 795-826, 4 October 2021.
+    </div>
+    <div class="rounded bg-base-200 p-2">
+      <h2 class="text-center text-xl">
+        <a
+          href="https://www.sciencedirect.com/science/article/pii/S0001870818303347"
+          >2-Groups, 2-characters, and Burnside rings</a
+        >
+      </h2>
+      <p class="p-2">
+        <a
+          class="link"
+          href="https://warwick.ac.uk/fac/sci/maths/people/staff/dmitriy_rumynin/"
+          >Dmitriy Rumynin</a
+        >, Alex Wendland.
+      </p>
       <a
         class="link-primary link"
         href="https://www.sciencedirect.com/science/article/pii/S0001870818303347"
-        >2-Groups, 2-characters, and Burnside rings</a
-      >. Advances in Mathematics, Volume 338, Pages 196-236, 7 November 2018.
-    </li>
-    <li>
-      Alex Wendland.
+        >Advances in Mathematics</a
+      >, Volume 338, Pages 196-236, 7 November 2018.
+    </div>
+    <div class="rounded bg-base-200 p-2">
+      <h2 class="text-center text-xl">
+        <a href="https://onlinelibrary.wiley.com/doi/abs/10.1002/jgt.22002">
+          Coloring of Plane Graphs with Unique Maximal Colors on Faces
+        </a>
+      </h2>
+      <p class="p-2">Alex Wendland.</p>
       <a
         class="link-primary link"
         href="https://onlinelibrary.wiley.com/doi/abs/10.1002/jgt.22002"
-        >Coloring of Plane Graphs with Unique Maximal Colors on Faces</a
-      >. Journal of graph theory, Volume 83, Issue 4, Pages 359-371, December
-      2016.
-    </li>
-  </ul>
+        >Journal of graph theory</a
+      >, Volume 83, Issue 4, Pages 359-371, December 2016.
+    </div>
+  </div>
 </template>
