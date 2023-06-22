0518d24983a69436919f4b87ee09acb1fe92ce9f
Alex Wendland <a.p.wendland@gmail.com>
2023-06-22 16:26:12 +0000

    Update project on coffee roulette

--- pages/projects/coffee_roulette.vue
+++ pages/projects/coffee_roulette.vue
@@ -1,10 +1,45 @@
 <template>
   <Project project_name="coffee_roulette">
-    <p class="pb-2">During my time in the Science and Engineering Faststream, a competitive leadership program in the civil service, I recognized the impact the COVID-19 crisis was having on the cohesion within our cohort. I identified a need for a tool that would facilitate connection and engagement between members to counter the isolation experienced during the pandemic. The task at hand was to develop a simple, engaging solution that would strengthen our network despite the challenging circumstances.</p>
+    <p class="pb-2">
+      During my time in the
+      <a
+        href="https://www.faststream.gov.uk/science-engineering/index.html"
+        class="link-primary link"
+        >Science and Engineering Faststream</a
+      >, a competitive leadership program in the civil service, I recognized the
+      impact the COVID-19 crisis was having on the cohesion within our cohort. I
+      identified a need for a tool that would facilitate connection and
+      engagement between members to counter the isolation experienced during the
+      pandemic. The task at hand was to develop a simple, engaging solution that
+      would strengthen our network despite the challenging circumstances.
+    </p>
 
-    <p class="pb-2">Using my technical skills and understanding of my cohort's needs, I developed a "Coffee Roulette" application. This innovative tool randomly paired members for weekly meetings, fostering connections and encouraging regular communication among our group. This required not only coding and design skills but also an understanding of the social dynamics within our cohort and how to foster positive interactions.</p>
+    <p class="pb-2">
+      Using my technical skills and understanding of my cohort's needs, I
+      developed a "Coffee Roulette" application. I made this in google sheets
+      using the google script in javascript. It involved
+      <a
+        href="https://docs.google.com/spreadsheets/d/1fwDNiu4zzg88fuT0ERlgydYoIhYX6vkL-yuVfBZzVD4/edit?usp=sharing"
+        class="link-primary link"
+        >a sheet</a
+      >
+      to manage and send the emails using the gmail plugin and a form for people
+      to sign up and manage their subscription. This innovative tool randomly
+      paired members for weekly meetings, fostering connections and encouraging
+      regular communication among our group. This required not only coding and
+      design skills but also an understanding of the social dynamics within our
+      cohort and how to foster positive interactions.
+    </p>
 
-    <p>The Coffee Roulette application was highly successful, significantly enhancing social connections within our group during a difficult period. Its success was such that I set up several similar networking initiatives within the broader Faststream community. One of these initiatives currently engages over 500 participants as of 2023, reflecting the effectiveness of this simple yet impactful solution in fostering engagement and collaboration among team members.</p>
+    <p>
+      The Coffee Roulette application was highly successful, significantly
+      enhancing social connections within our group during a difficult period.
+      Its success was such that people requested I set up several similar
+      networking initiatives within the broader Faststream community. One of
+      these initiatives currently engages over 500 participants as of 2023,
+      reflecting the effectiveness of this simple yet impactful solution in
+      fostering engagement and collaboration among team members.
+    </p>
   </Project>
 </template>
 
