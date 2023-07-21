edc17d218a5d3434ace01a80d5b067c55ca1f3a2
Alex Wendland <a.p.wendland@gmail.com>
2023-07-21 18:19:28 +0000

    Add Academic support description

--- pages/projects/academic_support.vue
+++ pages/projects/academic_support.vue
@@ -1,9 +1,75 @@
 <template>
   <Project project_name="academic_support">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      One of my most defining experiences during my time at the
+      <a href="https://warwick.ac.uk/" class="link link-primary">University of Warwick</a>
+      came from serving as the Academic Support Officer for the
+      <a href="https://www.warwicksu.com/societies-sports/societies/maths/" class="link link-primary">Warwick Maths Society</a>.
+      It was a period of intense dedication, hard work, and purposeful
+      commitment to strengthening the academic environment within our community.
+    </p>
+
+    <p class="pb-2">
+      My journey began with a simple yet effective initiative - I organized my
+      comprehensive collection of university notes and made them accessible to
+      all via
+      <a href="https://www.dropbox.com/sh/5m63moxv6csy8tn/AACcPd2m7UPHeGIfUNpH_iRza" class="link link-primary">Dropbox</a>.
+      This was a significant step towards enhancing the accessibility of notes
+      to students, particularly those who were unable to attend lectures.
+    </p>
+
+    <p class="pb-2">
+      As the Academic Support Officer, my role extended beyond mere note
+      sharing. One of my primary duties included orchestrating revision
+      lectures, both by soliciting the expertise of other esteemed members of
+      our academic society and, at times, delivering them myself. The experience
+      of standing before my peers, unraveling complex mathematical concepts, and
+      fostering a cooperative learning environment remains one of the most
+      rewarding aspects of my tenure.
+    </p>
+
+    <p class="pb-2">
+      With an aim to extend our academic support beyond the confines of
+      traditional lectures, I also ran weekly drop-in sessions. These became
+      a platform for students to voice their concerns, ask questions, and
+      actively engage in knowledge exchange outside the classroom. The scope of
+      my responsibility did not end there - I offered LaTeX support classes to
+      assist students with this typesetting system crucial for mathematical
+      documents, and organized lecture catch-up sessions for those who needed a
+      bit more assistance in keeping pace with our rigorous curriculum.
+    </p>
+
+    <p class="pb-2">
+      Recognizing the importance of a well-rounded university experience, I also
+      played an active role in other society activities such as social events,
+      academic talks, and career events. This enabled me to enhance our members'
+      university life in a balanced and holistic manner.
+    </p>
+
+    <p class="pb-2">
+      My tenure as the Academic Support Officer marked a turning point for the
+      Warwick Maths Society, one that witnessed a significant surge in academic
+      support activities. The year I was academic support we had the highest
+      recorded membership to date. The following year, the society had to split
+      my role into two separate positions to effectively manage the expanded
+      operations.
+    </p>
+
+    <p class="pb-2">
+      The positive feedback I received, particularly for my revision lectures,
+      has been incredibly heartening. Even years after delivering those
+      lectures, I continue to hear from former members who found them
+      instrumental in their academic journey. This reinforces my belief in the
+      importance of shared learning and peer-to-peer education.
+    </p>
+
+    <p>
+      Reflecting on my journey, I am grateful for the opportunity to contribute
+      significantly to enhancing education accessibility within the Warwick
+      Maths Society. My experience underscores the importance of fostering a
+      supportive academic environment, a principle that continues to shape my
+      approach towards learning and teaching today.
+    </p>
   </Project>
 </template>
 
