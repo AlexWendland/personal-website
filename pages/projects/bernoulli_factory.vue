0466abe79f78819f860459998980ec879dd5e22c
Alex Wendland <a.p.wendland@gmail.com>
2023-07-22 08:28:51 +0000

    Add Bernoulli factory project

--- pages/projects/bernoulli_factory.vue
+++ pages/projects/bernoulli_factory.vue
@@ -1,9 +1,50 @@
 <template>
   <Project project_name="bernoulli_factory">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      In the summer of 2013, I got funded by the
+      <a href="https://warwick.ac.uk/" class="link link-primary">University of Warwick</a>
+      to work under the guidance of
+      <a class="link link-primary" href="https://warwick.ac.uk/fac/sci/statistics/staff/academic-research/latuszynski/">
+        Krzysztof Łatuszyński</a>
+      on an intriguing mathematical problem.
+    </p>
+    <p class="pb-2">
+      Our problem was framed in the context of probability and had a seemingly
+      straightforward premise - given a coin, the probability of landing heads
+      or tails of which was unknown, how could we simulate other coins as a
+      function of the original one? For instance, if the unknown probability of
+      achieving heads was p, you could generate p<sup>2</sup> by tossing the
+      coin twice.
+    </p>
+    <p class="pb-2">
+      This seemingly simple problem turned out to be my gateway into the world
+      of university-level research, introducing me to the art of organizing my
+      time, tackling open-ended problems, and applying ideas gleaned from other
+      papers to our investigation.
+    </p>
+    <p class="pb-2">
+      Our exploration into this realm of uncertainty led us to devise an
+      innovative algorithm for rational functions. We utilized perfect sampling
+      with Markov chains, a method that hinged on the principles of probability
+      theory and statistics. As our work progressed, we found that our functions
+      could be used to construct an envelope of functions, which could, in turn,
+      estimate any other function.
+    </p>
+    <p class="pb-2">
+      In 2022, after years of rigorous exploration, our research came to
+      fruition. With the valuable assistance of
+      <a class="link link-primary" href="https://warwick.ac.uk/fac/sci/statistics/staff/research_students/morina/">
+        Giulio Morina</a>,
+      <a class="link link-primary" href="https://warwick.ac.uk/fac/sci/statistics/staff/academic-research/latuszynski/">
+        Krzysztof Łatuszyński</a>
+      Ph.D. student, we published our findings in our paper
+      <a href="https://projecteuclid.org/journals/annals-of-applied-probability/volume-32/issue-1/From-the-Bernoulli-factory-to-a-dice-enterprise-via-perfect/10.1214/21-AAP1679.short" class="link link-primary">
+        From the Bernoulli factory to a dice enterprise via perfect sampling of Markov
+        chains</a>.
+    </p>
+    <p class="pb-2">
+      Reflecting upon this formative journey, I realize the value this experience added to my academic growth. My first dive into research was not just about solving a mathematical problem; it was about embracing the unknown, persevering through challenges, and ultimately contributing to the broader scientific discourse. This experience continues to shape my approach to problem-solving and research today, nurturing in me an undying curiosity and a relentless pursuit of knowledge.
+    </p>
   </Project>
 </template>
 
