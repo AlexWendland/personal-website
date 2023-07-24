102eadc9a45264dab62b8f0d3852eacb5df05284
Alex Wendland <a.p.wendland@gmail.com>
2023-07-24 19:43:51 +0000

    Add CSaP project

--- pages/projects/csap.vue
+++ pages/projects/csap.vue
@@ -1,9 +1,59 @@
 <template>
   <Project project_name="csap">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      During my Ph.D. studies, I was fortunate to secure EPSRC funding for a
+      3-month internship at
+      <a href="https://www.csap.cam.ac.uk/" class="link link-primary">the Center for Science and Policy</a>
+      (CSaP), a prominent organization bridging the gap between governmental
+      agencies and university researchers to address pivotal policy questions.
+    </p>
+    <p class="pb-2">
+      My role at CSaP was two-fold. Firstly, it involved getting actively
+      involved in the day to day operations of the organisation: organising
+      events, facilitating meetings between policy officials and academics, and
+      writing stories about policy developments. Secondly, it required me to
+      apply my technical skills to develop tools to improve internal processes
+      and utilisation of CSaP's data within the organization.
+    </p>
+    <p>
+      In terms of content creation, I published a series of articles on CSaP's
+      platform:
+    </p>
+    <ul class="list-disc list-inside">
+      <li>
+        One piece explored <a href="https://www.csap.cam.ac.uk/news/article-how-business-learning-cooperate-pursuit-climate-ac/" class="link link-primary">how businesses were adapting to the urgent need for climate change mitigation</a>.
+      </li>
+      <li>
+        Another article delved into the <a href="https://www.csap.cam.ac.uk/news/article-what-online-manipulation-and-how-regulate-against-/" class="link link-primary">challenge of online manipulation and potential regulatory responses</a>.
+      </li>
+      <li>
+        Lastly, I tackled the complex issue of <a href="https://www.csap.cam.ac.uk/news/article-what-are-challenges-developing-new-approach-how-we/" class="link link-primary">how we assess the value of services that bring public good</a>.
+      </li>
+    </ul>
+    <p class="pb-2">
+      On the technical front, I embarked on a self-taught journey into Natural
+      Language Processing. I developed a model capable of matching policy
+      officials' questions to the appropriate academics based on their past
+      meetings, CSaP news stories written about them, and their professional
+      bios.
+    </p>
+    <p class="pb-2">
+      Additionally, recognizing the organization's need for effective data
+      management, I built several tools to help them access and retrieve
+      meaningful information from their database. This initiative not only
+      streamlined their operations but also generated significant cost savings
+      by reducing the need for temporary staff to generate infographics and
+      other data-heavy outputs.
+    </p>
+    <p>
+      The tools I developed significantly enhanced the efficiency of CSaP's
+      internal operations, leading to time and cost savings for the
+      organization. My contributions in content creation provided thoughtful
+      insights into relevant policy questions, fostering an informed dialogue
+      between government and academia. These experiences not only underscored
+      the importance of blending technology with policy discussions but also
+      solidified my commitment to using my skills for impactful problem-solving.
+    </p>
   </Project>
 </template>
 
