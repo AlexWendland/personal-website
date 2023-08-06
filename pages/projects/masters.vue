389929ac583c7782e38d4c3fa9934db08a23ba1e
Alex Wendland <a.p.wendland@gmail.com>
2023-08-06 14:37:41 +0000

    Add masters project

--- pages/projects/masters.vue
+++ pages/projects/masters.vue
@@ -1,9 +1,39 @@
 <template>
   <Project project_name="masters_thesis">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      For my masters thesis I worked with
+      <a class="link link-primary" href="https://warwick.ac.uk/fac/sci/maths/people/staff/dmitriy_rumynin/">Dmitriy Rumynin</a>
+      on an area of Maths called 2-Category theory. We wanted to make these
+      abstract objects more concrete via representation and character theory.
+    </p>
+
+    <p class="pb-2">
+      This project was worth one quarter of my final years grade but took a lot
+      more time than that. I had to read research level Mathematical papers and
+      reinterpret what I understood from the papers in to concrete terms. I
+      distinctly remember speaking to Dimitriy in one meeting, a little worn
+      down by the whole process and asking why it all had to be so complicated,
+      he said:
+      <Quote
+        quote="If it was more simple, it would be less beautiful."
+        author="Dmitriy Rumynin"
+        href="https://warwick.ac.uk/fac/sci/maths/people/staff/dmitriy_rumynin/"
+      />
+      So I soldiered on, in this "o so beautiful" world.
+    </p>
+
+    <p>
+      In the end my main achievement was writing down a concrete interpretation
+      of the
+      <a href="https://en.wikipedia.org/wiki/Shapiro%27s_lemma" class="link link-primary">Shapiro's lemma</a>.
+      This provides a relationship between two abstract objects, though this
+      relationship was abstractly constructed. I wrote down what the exact
+      relationship was in concrete terms that you could calculate and in doing
+      so proved the objects had a stronger relationship. This work got published
+      in
+      <a class="link-primary link" href="https://www.sciencedirect.com/science/article/pii/S0001870818303347">Advances in Mathematics</a>
+      in 2018.
+    </p>
   </Project>
 </template>
 
