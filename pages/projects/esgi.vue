3b21eb03853e05759204aa552f8fefab5152d844
Alex Wendland <a.p.wendland@gmail.com>
2023-08-06 14:05:14 +0000

    Add ESGI project

--- pages/projects/esgi.vue
+++ pages/projects/esgi.vue
@@ -1,9 +1,52 @@
 <template>
   <Project project_name="esgi">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      I attended the
+      <a href="https://ecmiindmath.org/european-study-groups-with-industry/" class="link link-primary">European Study Groups with Industry (ESGI)</a>
+      workshops during my PhD, first in the University of Bath in 2018 and then
+      in the
+      <a href="https://www.maths.cam.ac.uk/events/145th-mathematical-european-study-group-industry-esgi" class="link link-primary">University of Cambridge</a>
+      in 2019. These workshops provide a platform for academic mathematicians to
+      collaborate with industries on real-world challenges.
+    </p>
+
+    <p class="pb-2">
+      In Bath,
+      <a href="https://www.heathrow.com/" class="link link-primary">Heathrow Airport</a>
+      tasked us with exploring cost-saving opportunities via renovation project
+      scheduling. In Cambridge, our collaboration with the
+      <a href="https://www.gov.uk/government/organisations/defence-science-and-technology-laboratory" link="link link-primary">Defense Science and Technology Laboratory (DSTL)</a>
+      focused on strengthening image classifiers against adversarial attacks.
+    </p>
+
+    <p class="pb-2">
+      In Bath, using three years of Heathrow's project data, we identified
+      challenges like staff shortages, weather-related disruptions, and project
+      scope changes. Heathrow's project initiation patterns seemed arbitrary
+      within their 4-year spending cycle. We developed a model to forecast time
+      and cost overruns and assessed three scheduling strategies.
+    </p>
+
+    <p class="pb-2">
+      In Cambridge, we built image classifiers using PyTorch, categorizing
+      potential threats based on their type and familiarity. I worked on
+      adversarial training of a convolutional network model, ensuring it could
+      withstand image perturbations. The team also delved into techniques like
+      image pre-processing and attack detection.
+    </p>
+
+    <p class="pb-2">
+      For Heathrow, our scheduling strategies indicated potential savings up to
+      5% of their operational budget. In Cambridge, we found that while it's
+      possible to strengthen classifiers against minor image perturbations, they
+      remain vulnerable to more aggressive attacks due to the complexity of
+      image data.
+    </p>
+
+    <p>
+      These ESGI workshops highlighted the value of applying academic knowledge
+      to practical industry problems.
+    </p>
   </Project>
 </template>
 
