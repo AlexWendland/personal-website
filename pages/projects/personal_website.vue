c1af1656cfc59433ec2f80471f0ec3f4a255bd1f
Alex Wendland <a.p.wendland@gmail.com>
2023-08-12 06:57:00 +0000

    Add personal website project

--- pages/projects/personal_website.vue
+++ pages/projects/personal_website.vue
@@ -1,9 +1,53 @@
 <template>
   <Project project_name="personal_website">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      While I was at <NuxtLink class="link link-primary" to="/work#ccdata">CC-data</NuxtLink>, a baffling
+      thought struck me: What really happens when we enter a website's address
+      into our browser? Astonishingly, I didn't have a clear answer, even though
+      I was part of a tech firm with a primary product accessed via a website.
+    </p>
+
+    <p class="pb-2">
+      This sparked a curiosity in me. I wanted to delve deeper into the
+      intricate workings of the internet and truly grasp the mechanics of
+      websites. My goal was twofold: to host my own website, thereby enhancing
+      my understanding of the process, and to comprehend the challenges and
+      priorities of web developers.
+    </p>
+
+    <p class="pb-2">
+      I envisioned my website (this website!) as an interactive CV, doubling as
+      a platform for other projects. My aim was to gain at least foundational
+      knowledge across the entire web stack.
+    </p>
+
+    <p class="pb-2">
+      My choice for this endeavor was
+      <a href="https://nuxt.com/" class="link link-primary">Nuxt.js</a>. This
+      framework facilitates building
+      <a href="https://vuejs.org/" class="link link-primary">Vue.js</a>
+      applications seamlessly. One of its standout features is its unique
+      universal rendering system, which I believed would be resilient over time.
+      Moreover, its ease in page creation appealed to me, especially since my
+      <NuxtLink class="link link-primary" to="/projects/mimir">previous experience</NuxtLink> with
+      <a href="https://www.djangoproject.com/" class="link link-primary">Django</a>
+      felt somewhat cumbersome. However, one minor hiccup I encountered with
+      Nuxt.js was its limited plugin support, perhaps owing to its multi-layered
+      framework nature.
+    </p>
+
+    <p>
+      Embarking on this website creation journey has been enlightening and is an
+      ongoing learning curve for me. It's crystallized my understanding that, at
+      its core, the internet consists of API endpoints delivering HTML,
+      JavaScript, or raw data. Subsequently, it's the browser's responsibility
+      to weave this information into the websites we interact with. The
+      proliferation of mobile devices has added layers of complexity to web
+      development. Ensuring responsive design across devices can be challenging.
+      Nonetheless, tools like Google Chrome's developer utilities offer immense
+      support. With features like "Inspect", one can even playfully tweak major
+      websites, making the entire process quite engaging!
+    </p>
   </Project>
 </template>
 
