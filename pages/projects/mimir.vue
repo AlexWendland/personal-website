a796daa347d3f2aeeae3b36d159361363c20ca49
Alex Wendland <a.p.wendland@gmail.com>
2023-08-12 06:27:36 +0000

    Add project descriptions for Mimir and Obsidian

--- pages/projects/mimir.vue
+++ pages/projects/mimir.vue
@@ -1,12 +1,48 @@
 <template>
   <Project project_name="mimir">
-    Not fully written yet but here are some links from the project:
-    <a
-      href="https://www.youtube.com/watch?v=5J-rCk48gdI&ab_channel=SigmaWarwick"
-      >Original elevator pitch</a
-    >
-    <a href="https://mimirteam.wordpress.com/">Project blog</a>
-    <a href="https://www.jisc.ac.uk/">Organisers page</a>
+
+    <p class="pb-2">
+      During the summer of 2013,
+      <a href="https://www.jisc.ac.uk/">Organisers page</a><a href="https://www.jisc.ac.uk/" class="link link-primary">JISC</a>
+      presented an opportunity called the 'Summer of Student Innovation' to fund
+      tech-based startup ideas that benefited the common social good. The
+      selection was based on a public vote on submitted elevator pitch videos
+      (<a href="https://www.youtube.com/watch?v=5J-rCk48gdI&ab_channel=SigmaWarwick" class="link link-primary">here is ours</a>).
+    </p>
+
+    <p class="pb-2">
+      Together with three university friends, we envisioned a mathematics learning
+      website targeted at non-math university students who needed to grasp
+      foundational computational math techniques. Our unique selling point was an
+      error detection system for auto-generated questions, planned to be executed
+      through
+      <a href="https://www.djangoproject.com/" class="link link-primary">Django</a>,
+      a Python web framework.
+    </p>
+
+    <p class="pb-2">
+      We brought our vision to life, creating a prototype website named "Mimir"
+      (<a href="https://github.com/AlexWendland/mimir" class="link link-primary">code</a>)
+      that had a basic content offering. The standout feature, our error detection
+      system, functioned through a calculation graph. This graph delineated
+      potential mistakes students might make at every calculation stage, such as
+      sign errors or incorrect orderings. If a student answered incorrectly, our
+      system would trace their error using the graph and offer specific feedback.
+      Throughout this process, we documented our journey on a
+      <a href="https://mimirteam.wordpress.com/" class="link link-primary">project blog</a>
+      and ultimately showcased our prototype at a final event in London.
+    </p>
+
+    <p>
+      While this was my first foray into comprehensive web development, the
+      project's true value lay in the soft skills I acquired. Working with
+      friends, especially with one member being remote and having different work
+      habits, underscored the importance of effective communication and
+      collaboration. Even though we didn't pursue the project further, it honed
+      my abilities in project and time management, and it also taught me the
+      crucial skill of determining priorities for an initial product release.
+    </p>
+
   </Project>
 </template>
 
--- pages/projects/obsidian.vue
+++ pages/projects/obsidian.vue
@@ -1,9 +1,49 @@
 <template>
   <Project project_name="obsidian">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia itaque
-    maxime adipisci, ducimus ratione ad laudantium accusantium alias, fugit
-    veniam possimus laborum debitis at omnis. Quia nulla eius distinctio
-    accusamus?
+    <p class="pb-2">
+      Once I transitioned to a professional setting, it quickly became apparent
+      that I needed to elevate my organizational skills. This wasn't just
+      because I found my memory to be somewhat unreliable, but I also felt a
+      strong desire to bring more structure and direction to my life. While my
+      academic journey inherently provided a framework, the working world
+      necessitated a more self-driven approach.
+    </p>
+
+    <p>
+      Enter <b>Obsidian</b>.
+    </p>
+
+    <Quote
+      quote="Obsidian is the private and flexible note‑taking app that adapts to the way you think."
+      author="Obsidian"
+      href="https://obsidian.md/"
+    />
+
+    <p class="pb-2">
+      While it aligns with other note-taking apps like
+      <a href="https://www.notion.so/" class="link link-primary">Notion</a> or
+      <a href="https://www.microsoft.com/en-gb/microsoft-365/onenote/digital-note-taking-app" class="link link-primary">One-Note</a>,
+      Obsidian stands apart with its local hosting using markdown files.
+      Furthermore, thanks to community plugins, it offers powerful
+      organizational capabilities—from SQL-like note querying and automated
+      review systems to fostering a space for free-flowing idea exploration.
+    </p>
+
+    <p class="pb-2">
+      I've taken the initiative to host a
+      <a href="https://obsidian.awendland.co.uk" class="link link-primary">public vault</a>,
+      which is a curated collection of my notes and insights as I journey
+      through my development career. Think of it as a peek into my cognitive
+      realm, where I synthesize information from varied sources, with a keen
+      focus on my core passions: programming, mathematics, and philosophy.
+    </p>
+
+    <p class="pb-2">
+      As time progresses, my ambition is for this vault to not only serve as a
+      personal repository but also as a valuable resource for those who share a
+      similar backstory and path to mine.
+    </p>
+
   </Project>
 </template>
 
